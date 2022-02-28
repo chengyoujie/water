bf4cbd71bb62bfa5070ee87ee189fa794aa194ae
cyj <1137815160@qq.com>
2022-02-28 21:28:43 +0800

    修改图片名字长度

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,14 @@ module.exports = {
         },
         {
             test:/\.(png|jpg)$/,
-            use:['file-loader']
+            
+            use:[{
+                loader:'file-loader',
+                options:{
+                    name:"img/[name].[hash:8].[ext]"
+                },
+            }]
+            
         }
         ],
     },
